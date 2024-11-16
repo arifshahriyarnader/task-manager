ce532ea0d6f3de11ba6d07f74a028c22b8dad7fe
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-16 16:47:23 +0600

    get one task and all taks

--- routes/api/task.js
+++ routes/api/task.js
@@ -16,4 +16,32 @@ router.post('/', authenticateToken,  async(req,res)  =>{
     }
 })
 
+//gets all tasks
+router.get('/', authenticateToken, async(req,res) =>{
+    try{
+        const getAllTasks= await Task.find({userId:req.user._id})
+        res.json(getAllTasks)
+    }
+    catch(error){
+        res.status(500).json({message:"Something went wrong"})
+    }
+})
+
+//gets one task
+router.get('/:id', authenticateToken, async(req,res) =>{
+    try{
+        const id=req.params.id;
+        const task=await Task.findById(id);
+        if(task){
+            return res.json(task);
+        }
+        else{
+            return res.status(400).json({message:"Task not found"})
+        }
+    }
+    catch(error){
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
 module.exports=router;
