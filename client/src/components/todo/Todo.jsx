c749e81cf53f6b260ddb0487876ab556c475c573
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-12-17 16:42:18 +0600

    task delete item added

--- client/src/components/todo/Todo.jsx
+++ client/src/components/todo/Todo.jsx
@@ -1,85 +1,98 @@
-import React, { useState } from 'react'
-import TodoCard from './TodoCard';
+import React, { useState } from "react";
+import TodoCard from "./TodoCard";
 
 const Todo = () => {
-  const [newTodoTitle, setNewTodoTitle] =useState('');
-  const [newTodoDescription, setNewTodoDescription] =useState('');
-  const [todoLists,setTodoLists] =useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [newTodoDescription, setNewTodoDescription] = useState("");
+  const [todoLists, setTodoLists] = useState([]);
 
-  const handleTitleChange=(e)=> setNewTodoTitle(e.target.value)
+  const handleTitleChange = (e) => setNewTodoTitle(e.target.value);
 
-  const handleDescriptionnChange=(e) => setNewTodoDescription(e.target.value)
-  
-  const handleClick=() =>{
+  const handleDescriptionnChange = (e) => setNewTodoDescription(e.target.value);
+
+  const handleClick = () => {
     //avoid adding empty todos
-    if(!newTodoTitle || !newTodoDescription){
+    if (!newTodoTitle || !newTodoDescription) {
       return;
     }
     //add the new todo list
-    const newTodo={title:newTodoTitle, description:newTodoDescription};
-      setTodoLists([...todoLists,newTodo])
+    const newTodo = { title: newTodoTitle, description: newTodoDescription };
+    setTodoLists([...todoLists, newTodo]);
 
-      //clear the input fields
-      setNewTodoTitle('');
-      setNewTodoDescription('');
-  }
+    //clear the input fields
+    setNewTodoTitle("");
+    setNewTodoDescription("");
+  };
+
+  const handleDelete = (index) => {
+    todoLists.splice(index, "1");
+    setTodoLists([...todoLists]);
+  };
   return (
-    <div className='min-h-screen bg-gray-100'>
-    <div className="flex items-center justify-center">
-    <form
-      className="w-full max-w-md bg-white p-8 mt-20 shadow-md rounded-md"
-    >
-      <h2 className="text-2xl font-bold text-center mb-6">Add Todo</h2>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center">
+        <form className="w-full max-w-md bg-white p-8 mt-20 shadow-md rounded-md">
+          <h2 className="text-2xl font-bold text-center mb-6">Add Todo</h2>
 
-      <div className="mb-4">
-        <label htmlFor="title" className="block text-gray-700 font-medium mb-1">
-          Title
-        </label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-        value={newTodoTitle}
-        onChange={handleTitleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-          placeholder="Enter a title"
-          required
-        />
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-gray-700 font-medium mb-1"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={newTodoTitle}
+              onChange={handleTitleChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+              placeholder="Enter a title"
+              required
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 font-medium mb-1"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={newTodoDescription}
+              onChange={handleDescriptionnChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+              placeholder="Enter a description"
+              rows="4"
+              required
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            onClick={handleClick}
+            className="w-full bg-orange-500 text-white py-2 px-4 rounded-md font-bold hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500"
+          >
+            Add Todo
+          </button>
+        </form>
       </div>
-
-      <div className="mb-6">
-        <label htmlFor="description" className="block text-gray-700 font-medium mb-1">
-          Description
-        </label>
-        <textarea
-          id="description"
-          name="description"
-          value={newTodoDescription}
-          onChange={handleDescriptionnChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-          placeholder="Enter a description"
-          rows="4"
-          required
-        ></textarea>
+      <div className="w-full px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {todoLists.map((todo, index) => (
+          <TodoCard
+            key={index}
+            title={todo.title}
+            description={todo.description}
+            onDelete={() => handleDelete(index)}
+          />
+        ))}
       </div>
-
-      <button
-        type="submit"
-        onClick={handleClick}
-        className="w-full bg-orange-500 text-white py-2 px-4 rounded-md font-bold hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500"
-      >
-        Add Todo
-      </button>
-    </form>
-  </div>
-  <div className='w-full px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-    {todoLists.map((todo,index) =>(
-        <TodoCard key={index} title={todo.title} description={todo.description} />
-    ))}
- </div>
- </div>
-  
-  )
-}
+    </div>
+  );
+};
 
 export default Todo;
--- client/src/components/todo/TodoCard.jsx
+++ client/src/components/todo/TodoCard.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { FaTrashAlt } from 'react-icons/fa';
-import { MdEdit } from 'react-icons/md'; 
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
 
-const TodoCard = ({title,description}) => {
+const TodoCard = ({ title, description, onDelete }) => {
   return (
-    <div className='my-8 bg-white shadow-md rounded-md border border-orange-300 p-4 text-center'>
-     <h3 className='text-lg'>{title}</h3>
-     <p className='text-lg break-words'>{description}</p>
-     <div className='flex flex-row justify-between cursor-pointer'>
-      <div className='text-orange-600'>
-        <MdEdit />
+    <div className="my-8 bg-white shadow-md rounded-md border border-orange-300 p-4 text-center">
+      <h3 className="text-lg">{title}</h3>
+      <p className="text-lg break-words">{description}</p>
+      <div className="flex flex-row justify-between cursor-pointer">
+        <div className="text-orange-600">
+          <MdEdit />
+        </div>
+        <div className="text-orange-600" onClick={onDelete}>
+          <FaTrashAlt />
+        </div>
       </div>
-      <div className='text-orange-600'>
-        <FaTrashAlt />
-      </div>
-     </div>
     </div>
-  )
-}
+  );
+};
 
 export default TodoCard;
